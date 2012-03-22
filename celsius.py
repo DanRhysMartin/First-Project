cd464717b788cc68af1b033f161cbd86af1e3e78
dam42 <dan-killer-driller@hotmail.co.uk>
2012-03-22 21:36:16 +0000

    celsius to fahrenheit

---
+++ celsius.py
@@ -0,0 +1,10 @@
+print "For Fahrenheit to Celsius, Press 1. For Celsius To Fahrenheit, Press 2."
+x = raw_input("1 or 2: ")
+if x == "1":
+    F = input("Fahrenheit: ")
+    C = (F - 32)*5/9
+    print C
+elif x == "2":
+    C = input("Celsius: ")
+    F = C*9/5 + 32
+    print F 

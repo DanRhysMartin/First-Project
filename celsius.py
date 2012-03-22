78df31eec4e4c3b46ca86369c1cae5855ad9b7c7
dam42 <dan-killer-driller@hotmail.co.uk>
2012-03-22 21:45:00 +0000

    while loop

--- celsius.py
+++ celsius.py
@@ -1,10 +1,11 @@
-print "For Fahrenheit to Celsius, Press 1. For Celsius To Fahrenheit, Press 2."
-x = raw_input("1 or 2: ")
-if x == "1":
-    F = input("Fahrenheit: ")
-    C = (F - 32)*5/9
-    print C
-elif x == "2":
-    C = input("Celsius: ")
-    F = C*9/5 + 32
-    print F 
+while 1: 
+    print "For Fahrenheit to Celsius, Press 1. For Celsius To Fahrenheit, Press 2."
+    x = raw_input("1 or 2: ")
+    if x == "1":
+        F = input("Fahrenheit: ")
+        C = (F - 32)*5/9
+        print C
+    elif x == "2":
+        C = input("Celsius: ")
+        F = C*9/5 + 32
+        print F 
